3186b244b8a29a1b762cba3063600fb2d2a10609
HackersiBogart <arjaydelafuente87@gmail.com>
2024-11-25 10:15:04 +0800

    fix alignment of email commented out email form

--- app/mailers/email_mailer.rb
+++ app/mailers/email_mailer.rb
@@ -3,6 +3,23 @@ class EmailMailer < ApplicationMailer
 
   def send_email(receiver_email)
     @receiver_email = receiver_email
-    mail(to: @receiver_email, subject: 'Hello from Rails App', body: 'This is a test email from your Rails app!')
+    mail(
+      to: @receiver_email, 
+      subject: 'Your Order Has Been Fulfilled! 🎉',
+      body: <<~BODY
+        Dear Customer,
+
+        We are delighted to inform you that your order has been successfully fulfilled and is ready for you! 🎉
+
+        Your order is now ready for pickup at our store:
+        TJ Paint Center
+        Vinzons Ave., Daet, Camarines Norte
+
+        Want to shop with us again? Click this link: https://tjims-ckom.onrender.com
+
+        Warm regards,
+        TJ Stock Optima
+      BODY
+    )
   end
 end
