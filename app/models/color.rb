9eacc19df1d60c6306154764f7996adc2dd1f4db
HackersiBogart <arjaydelafuente87@gmail.com>
2024-11-28 22:46:11 +0800

    add validation for colors

--- app/models/color.rb
+++ app/models/color.rb
@@ -5,6 +5,9 @@ class Color < ApplicationRecord
   has_many :select
   has_many :paint_colors
   has_many :primary_colors
+  validates :name, presence: true
+  validates :code, presence: true
+  validates :image, presence: true
 
 
 
