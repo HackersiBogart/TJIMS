8c6d585ce293265da727000bc904d599fd36c0ff
HackersiBogart <arjaydelafuente87@gmail.com>
2024-12-05 22:26:24 +0800

    change carts controller again

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,7 +1,10 @@
 class CartsController < ApplicationController
   def show
-    @paint_color = PaintColor.includes(:color, :product, :stocks)
-    .find(params[:id])
+   
+    @paint_color = PaintColor.find_by(id: params[:id])
+    @color = Color.find_by(id: params[:id])
+    @product = Product.find_by(id: params[:id])
+
   end
 
 end
