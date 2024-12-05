d0c8caa2e3481a9ca52893a016781175f2673ce8
HackersiBogart <arjaydelafuente87@gmail.com>
2024-12-06 00:39:24 +0800

    add gone features

--- app/javascript/controllers/paint_colors_controller.js
+++ app/javascript/controllers/paint_colors_controller.js
@@ -86,4 +86,46 @@ export default class extends Controller {
     // Provide feedback to the user
     alert("Item removed from cart.");
   }
+
+  selectSize(e) {
+    this.sizeValue = e.target.value
+    const selectedPrice = e.target.dataset.price // Get the price from the selected size option
+
+    const selectedSizeEl = document.getElementById("selected-size")
+    selectedSizeEl.innerText = `Selected Size: ${this.sizeValue}`
+
+    this.updatePrice(selectedPrice) // Update the price when a new size is selected
+  }
+
+  selectUnit(e) {
+    this.unitValue = e.target.value
+    const selectedUnitEl = document.getElementById("selected-unit")
+    selectedUnitEl.innerText = `Selected Unit: ${this.unitValue}`
+
+    // Filter the size options based on the selected unit
+    const sizeSelect = document.getElementById("size-select")
+    const sizeOptions = sizeSelect.querySelectorAll("option")
+
+    sizeOptions.forEach(option => {
+      if (option.dataset.unit === this.unitValue || option.value === "") {
+        option.style.display = "block" // Show matching sizes
+      } else {
+        option.style.display = "none" // Hide sizes that don't match the selected unit
+      }
+    })
+
+    this.updateUnit(this.unitValue) // Update the unit when a new unit is selected
+  }
+
+  updatePrice(price) {
+    // Update the price displayed on the page
+    this.priceDisplay.innerHTML = `₱${parseFloat(price).toFixed(2)}`
+    this.priceValue = parseFloat(price) // Ensure the priceValue is updated
+  }
+
+  updateUnit(unit) {
+    // Update the unit displayed on the page
+    this.unitDisplay.innerHTML = `Unit: ${unit}`
+  }
 }
+
