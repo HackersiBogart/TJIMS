d201d8065e849773a8703d98f94d492c9b683fee
HackersiBogart <arjaydelafuente87@gmail.com>
2024-12-08 23:13:36 +0800

    fix paint_colors_controller

--- app/controllers/paint_colors_controller.rb
+++ app/controllers/paint_colors_controller.rb
@@ -1,7 +1,13 @@
 class PaintColorsController < ApplicationController
 def show
 
-  @paint_color = PaintColor.includes(:color, :product).find(params[:id])
+  @paint_color = PaintColor.find(params[:id])
+  # Ensure these associations exist
+  @paint_color.color_id ||= @paint_color.color&.id
+  @paint_color.product_id ||= @paint_color.product&.id
+
+  logger.debug "Color ID: #{@paint_color.color_id}"
+  logger.debug "Product ID: #{@paint_color.product_id}"
 
 end
 
