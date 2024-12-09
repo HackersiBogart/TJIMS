6d9492c2cbef41e7e2040d7dc197017887ed9926
HackersiBogart <arjaydelafuente87@gmail.com>
2024-12-09 12:18:43 +0800

    edit imprting js

--- app/javascript/controllers/customer_order_controller.js
+++ app/javascript/controllers/customer_order_controller.js
@@ -1,4 +1,4 @@
-import { Controller } from "stimulus";
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   static targets = ["product", "paintColor"];  // Ensure 'product' is included here
