bb3249232f470bf45ee226d15c7f56f89628a6ef
HackersiBogart <arjaydelafuente87@gmail.com>
2024-12-09 10:41:00 +0800

    fix carts_controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,10 +1,13 @@
 class CartsController < ApplicationController
   def show
-   
-    @paint_color = PaintColor.find_by(id: params[:id])
-    @color = Color.find_by(id: params[:id])
-    @product = Product.find_by(id: params[:id])
+    @paint_color = PaintColor.find_by(id: params[:id]) # Find the paint color by ID
 
+    if @paint_color
+      @color = @paint_color.color    # Retrieve associated color
+      @product = @paint_color.product # Retrieve associated product
+    else
+      flash[:error] = "Paint color not found"
+      redirect_to root_path # Or any fallback path
+    end
   end
-
 end
