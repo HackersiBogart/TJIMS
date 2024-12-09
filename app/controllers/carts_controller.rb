ef2048b1324a3796682feeb5c4d1f36974ad4ca3
HackersiBogart <arjaydelafuente87@gmail.com>
2024-12-09 11:36:03 +0800

    rollback changes

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,14 +1,10 @@
 class CartsController < ApplicationController
   def show
-    @paint_color = PaintColor.includes(:color, :product).find(params[:id]) # Eager load associations
-  
-    # Ensure associated IDs are set
-    if @paint_color.color_id.nil? && @paint_color.color.present?
-      @paint_color.color_id = @paint_color.color.id
-    end
-  
-    if @paint_color.product_id.nil? && @paint_color.product.present?
-      @paint_color.product_id = @paint_color.product.id
-    end
+   
+    @paint_color = PaintColor.find_by(id: params[:id])
+    @color = Color.find_by(id: params[:id])
+    @product = Product.find_by(id: params[:id])
+
   end
+
 end
