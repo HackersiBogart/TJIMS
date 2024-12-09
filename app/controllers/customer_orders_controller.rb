d201661bc7b24f9678e8f894bd3db9ff3895729d
HackersiBogart <arjaydelafuente87@gmail.com>
2024-12-09 13:08:54 +0800

    rollback changes

--- app/controllers/customer_orders_controller.rb
+++ app/controllers/customer_orders_controller.rb
@@ -24,7 +24,6 @@ class CustomerOrdersController < ApplicationController
   # GET /customer_orders/new
   def new
     @customer_order = CustomerOrder.new
-    @colors = Color.all  # Ensure you have a Color model and fetch the colors
   end
 
   # GET /customer_orders/1/edit
