d6369b6f1c0587b509527c4d9fb363a0b7e506c0
HackersiBogart <arjaydelafuente87@gmail.com>
2024-12-09 11:30:25 +0800

    update again

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,25 +1,14 @@
 class CartsController < ApplicationController
   def show
-    @paint_color = PaintColor.includes(:color, :product).find_by(id: params[:id])
-
-    if @paint_color.nil?
-      render json: { error: "Paint color not found" }, status: :not_found 
+    @paint_color = PaintColor.includes(:color, :product).find(params[:id]) # Eager load associations
+  
+    # Ensure associated IDs are set
+    if @paint_color.color_id.nil? && @paint_color.color.present?
+      @paint_color.color_id = @paint_color.color.id
     end
-
-    @color = @paint_color.color
-    @product = @paint_color.product
-
-    if @paint_color.color_id.nil? && @color.present?
-      @paint_color.update(color_id: @color.id)
-    end
-
-    if @paint_color.product_id.nil? && @product.present?
-      @paint_color.update(product_id: @product.id)
-    end
-
-    respond_to do |format|
-      format.html # Default to rendering HTML
-      format.json { render json: { paint_color: @paint_color, color: @color, product: @product } }
+  
+    if @paint_color.product_id.nil? && @paint_color.product.present?
+      @paint_color.product_id = @paint_color.product.id
     end
   end
 end
