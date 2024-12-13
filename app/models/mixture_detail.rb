1deb3cb0aa5fe5b1469bbb9f87c7a8812bc16ac6
HackersiBogart <arjaydelafuente87@gmail.com>
2024-12-13 13:46:39 +0800

    remove validations

--- app/models/mixture_detail.rb
+++ app/models/mixture_detail.rb
@@ -1,8 +1,7 @@
 class MixtureDetail < ApplicationRecord
   belongs_to :mixture
   belongs_to :primary_color
-  validates :mixture_id, presence: true
-  validates :order_id, presence: true, numericality: { only_integer: true }
+
   
 
 end
